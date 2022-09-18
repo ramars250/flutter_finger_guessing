a1f1e79237485d5e260170205000b4a181637ad5
mars.lee <ramars.tw@yahoo.com.tw>
2022-09-18 22:05:07 +0800

    大致完成，剩計分

--- lib/main.dart
+++ lib/main.dart
@@ -22,23 +22,30 @@ class FingerGuessing extends StatefulWidget {
 }
 
 class _FingerGuessing extends State<FingerGuessing> {
-  int cpuIndex = Random().nextInt(3) + 1;
+  //電腦方變數
+  int cpuIndex = 1;
 
+  //用於獲取點選後的變數
+  int tapIndex = 0;
+
+  //電腦方隨機函數
   void cpuRandom() {
     setState(() {
       cpuIndex = Random().nextInt(3) + 1;
+      // print(cpuIndex);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    int tapIndex = 0;
     return Column(
       children: [
+        //電腦方文字
         Container(
           padding: const EdgeInsets.only(top: 10),
           child: const Text('CPU'),
         ),
+        //電腦方圖片
         Container(
           // color: Colors.green,
           padding: const EdgeInsets.all(20),
@@ -57,22 +64,37 @@ class _FingerGuessing extends State<FingerGuessing> {
                 itemBuilder: (context, i) {
                   int index = i + 1;
                   return Image.asset('assets/images/blue$index.png',
-                        fit: BoxFit.contain);
+                      fit: BoxFit.contain);
                 },
               );
             },
           ),
         ),
-        Container(
+        //中間對決處
+        SizedBox(
           height: MediaQuery.of(context).size.height / 3,
           width: MediaQuery.of(context).size.width,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Container(
-                child: const Text(
-                  'CPU',
-                  style: TextStyle(fontSize: 16),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: Image.asset(
+                        'assets/images/blue$cpuIndex.png',
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    const Text(
+                      'CPU',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
                 ),
               ),
               Container(
@@ -81,24 +103,42 @@ class _FingerGuessing extends State<FingerGuessing> {
                   style: TextStyle(fontSize: 30),
                 ),
               ),
-              Container(
-                child: const Text(
-                  'YOU',
-                  style: TextStyle(fontSize: 16),
+              Padding(
+                padding: const EdgeInsets.only(right: 20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 100,
+                      height: 100,
+                      child: tapIndex != 0
+                          ? Image.asset(
+                              'assets/images/red$tapIndex.png',
+                              fit: BoxFit.contain,
+                            )
+                          : Image.asset(
+                              'assets/images/red1.png',
+                              fit: BoxFit.contain,
+                            ),
+                    ),
+                    const Text(
+                      'YOU',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
                 ),
               ),
             ],
           ),
         ),
-        // Container(
-        //   child: const
-          const Text('你的選擇'),
-        // ),
+        //我方文字
+        const Text('你的選擇'),
+        //我方圖片
         Container(
-          // color: Colors.green,
           padding: const EdgeInsets.all(20),
           height: MediaQuery.of(context).size.height / 6,
           width: MediaQuery.of(context).size.width,
+          //用StatefulBuilder是為了下方點選後可以正常變更顏色
           child: StatefulBuilder(
             builder: (context, setState) {
               return GridView.builder(
@@ -112,31 +152,35 @@ class _FingerGuessing extends State<FingerGuessing> {
                 itemBuilder: (context, i) {
                   int index = i + 1;
                   bool tapped = index == tapIndex;
+                  //可點選
                   return InkWell(
-                      onTap: () {
-                        setState(() {
-                          tapIndex = index;
-                        });
-                      },
-                      child: Container(
-                        child: tapped
-                            ? Image.asset(
-                                'assets/images/red$index.png',
-                                fit: BoxFit.contain,
-                              )
-                            : Image.asset(
-                                'assets/images/$index.jpg',
-                                fit: BoxFit.contain,
-                              ),
-                      ));
+                    onTap: () {
+                      setState(() {
+                        tapIndex = index;
+                      });
+                    },
+                    child: Container(
+                      child: tapped
+                          ? Image.asset(
+                              'assets/images/red$index.png',
+                              fit: BoxFit.contain,
+                            )
+                          : Image.asset(
+                              'assets/images/$index.jpg',
+                              fit: BoxFit.contain,
+                            ),
+                    ),
+                  );
                 },
               );
             },
           ),
         ),
+        //點擊後可開始對決
         ElevatedButton(
           onPressed: () {
             cpuRandom();
+            // print(tapIndex);
           },
           child: const Text('Test'),
         ),
